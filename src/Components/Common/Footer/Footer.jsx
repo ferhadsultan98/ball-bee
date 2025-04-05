5d282efeb473bd09316ca374d3f431d7857ef902
Farhad Sultanov <sultanovv991@gmail.com>
2025-04-05 13:15:43 +0400

    Update Footer.jsx

--- src/Components/Common/Footer/Footer.jsx
+++ src/Components/Common/Footer/Footer.jsx
@@ -38,7 +38,7 @@ const Footer = () => {
         </div>
       </div>
       <div className="footer-bottom">
-        <p>&copy; 2025 Ball-Bee. {t("footer.bottom")}.</p>
+        <p>&copy; 2025 BalBee. {t("footer.bottom")}.</p>
       </div>
     </footer>
   );
